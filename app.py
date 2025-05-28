3966ac14d489c4ed04723778a0986b83af0b1c4e
AlejaMontiel <167145236+AlejaMontiel@users.noreply.github.com>
2025-05-28 11:24:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -204,7 +204,6 @@ if img is not None:
 st.markdown('<p class="giant-title">BrachyCervix</p>', unsafe_allow_html=True)
 st.markdown("""
 st.markdown("""
-""", unsafe_allow_html=True)
 <hr>
 <div style="text-align:center;color:#28aec5;font-size:50px;">
     BrachyCervix - Semiautomátización y visor para procesos de braquiterapia enfocados en el Cervix
